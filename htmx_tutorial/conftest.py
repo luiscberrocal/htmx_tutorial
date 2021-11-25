5d2dc43cad721faa6bd3fd5e2b182ff92b717eeb
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2021-11-24 19:08:18 -0500

    First commit

---
+++ htmx_tutorial/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from htmx_tutorial.users.models import User
+from htmx_tutorial.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
