2c875796861edf42fc49a1ca755bf5286cc07330
Luis C. Berrocal <luis.berrocal@adelantos.com.pa>
2021-11-28 14:44:38 -0500

    [2] Added form to validate

---
+++ htmx_tutorial/clients/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+from htmx_tutorial.clients.models import Client
+
+
+class SimpleClientForm(forms.Form):
+    input_text = forms.CharField()
+    first_name = forms.CharField(required=False)
+    last_name = forms.CharField(required=False)
+
+    def clean(self):
+        cleaned_data = super(SimpleClientForm, self).clean()
+        input_text = cleaned_data['input_text'].title()
+        if ',' in input_text:
+            parts = input_text.split(',')
+            if len(parts) == 2:
+                cleaned_data['last_name'] = parts[0]
+                cleaned_data['first_name'] = parts[1]
+            else:
+                msg = 'More than one comma'
+                raise ValidationError(msg)
+        else:
+            parts = input_text.split(' ')
+            if len(parts) < 2:
+                msg = 'Need to include first and last name'
+                raise ValidationError(msg)
+            else:
+                cleaned_data['last_name'] = parts[-1]
+                cleaned_data['first_name'] = ' '.join(parts[:-1])
+        return cleaned_data
+
+    def save(self):
+        data = self.cleaned_data
+        client = Client.objects.get_or_create(first_name=data['first_name'], last_name=data['last_name'])
+        return client
+
