5806f261f8f75efd2b3cd154cf069d2c2209fb8a
Luis C. Berrocal <luis.berrocal@adelantos.com.pa>
2021-11-30 07:33:55 -0500

    [4] Added admin

--- htmx_tutorial/clients/admin.py
+++ htmx_tutorial/clients/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from htmx_tutorial.clients.models import Client
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ( 'id', 'last_name', 'first_name', 'display_order')
